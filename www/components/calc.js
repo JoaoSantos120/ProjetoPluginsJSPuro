d2718d623e441ef959c40e45276cf45008091d6d
JoaoSantos120 <joaohenriqstos12@gmail.com>
2021-10-01 12:22:12 +0900

    Project's Initial Commit

---
+++ www/components/calc.js
@@ -0,0 +1,35 @@
+// This is a JavaScript file
+
+window.onload = function (){
+  document.querySelector("alerta1").addEventListener("click", function(){
+    function retorno (){
+
+    }
+    navigator.notification.alert(
+      "Alerta de teste",
+       retorno,
+       "titulo de alerta",
+       "SIM");
+  });
+
+  document.querySelector("#alerta2").addEventListener("click", function(){
+      function verifica(buttonIndex){
+        if (buttonIndex == 1){
+          navigator.notification.alert("Escolheu a opção A")
+        }
+        else {
+          navigator.notification.alert("Escolheu a opção B")
+        }
+      }
+      navigator.notification.confirm(
+      "Escolha A ou B",
+      verifica,
+      "Opções de escolha",
+      ['A','B']
+    );
+});
+
+document.querySelector("#beep").addEventListener("click", function(){
+  navigator.notification.beep(2)
+});
+}
